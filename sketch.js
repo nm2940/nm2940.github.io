5712985188cab6691a98798cb2c274b00189fde9
nm2940 <nm2940@columbia.edu>
2018-02-07 18:20:38 -0500

    test

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+	background('red');
+	fill(255,255,0);
+	ellipse(300, 200, 80, 80);
+	rect(270,240,60,120);
+	textSize(40);
+	fill(0, 150, 255);
+	text('Hello World', 180, 450);
+}
+
+
