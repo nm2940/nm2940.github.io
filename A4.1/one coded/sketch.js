0406b2678a2773915174023f09d3433b4e9f01c3
nm2940 <nm2940@columbia.edu>
2018-02-28 17:11:04 -0500

    test

--- A4.1/one coded/sketch.js
+++ A4.1/one coded/sketch.js
@@ -9,6 +9,24 @@ var hh = 1280;
 var zoom = 12;
 var collision;
 
+
+
+    var type1=0;
+    var type2=0;
+    var type3=0;
+    var type4=0;
+    var type5=0;
+    var type6=0;
+
+
+    var l_type1=0;
+    var l_type2=0;
+    var l_type3=0;
+    var l_type4=0;
+    var l_type5=0;
+    var l_type6=0;
+
+
 function preload() {
   // The clon and clat in this url are edited to be in the correct order.
   mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
@@ -43,6 +61,10 @@ function setup() {
 
   var cx = mercX(clon);
   var cy = mercY(clat);
+  
+
+
+
 
   for (var i = 1; i < collision.length; i++) {
     var data = collision[i].split(/,/);
@@ -59,22 +81,41 @@ function setup() {
     }
 
 
-
     if(data[17]== "7AM"){
       fill(255, 255, 0, 40);
       noStroke();
       ellipse(x, y, 8,8);
+
+      if(data[4]=="Noise - Vehicle"){
+        type1=type1+1;
+      }
+      if(data[6]=="Club/Bar/Restaurant"){
+        l_type1=l_type1+1;
+      }
     }
 
     if(data[17]== "8AM"){
       fill(255, 200, 0, 40);
       noStroke();
       ellipse(x, y, 8,8);
+
+      if(data[4]=="Noise - Vehicle"){
+        type2=type2+1;
+      }
+      if(data[6]=="Club/Bar/Restaurant"){
+        l_type2=l_type2+1;
+      }
     }
     if(data[17]== "9AM"){
       fill(255, 150, 0, 40);
       noStroke();
       ellipse(x, y, 8,8);
+      if(data[4]=="Noise - Vehicle"){
+        type3=type3+1;
+      }
+      if(data[6]=="Club/Bar/Restaurant"){
+        l_type3=l_type3+1;
+      }
     }
 
 
@@ -82,20 +123,41 @@ function setup() {
       fill(255, 100, 0, 40);
       noStroke();
       ellipse(x, y, 8,8);
+      if(data[4]=="Noise - Vehicle"){
+        type4=type4+1;
+      }
+      if(data[6]=="Club/Bar/Restaurant"){
+        l_type4=l_type4+1;
+      }
     }
     if(data[17]== "6PM"){
       fill(255, 50, 0, 40);
       noStroke();
       ellipse(x, y, 8,8);
+      if(data[4]=="Noise - Vehicle"){
+        type5=type5+1;
+      }
+      if(data[6]=="Club/Bar/Restaurant"){
+        l_type5=l_type5+1;
+      }
     }
     if(data[17]== "7PM"){
       fill(255, 0, 0, 40);
       noStroke();
       ellipse(x, y, 8,8);
+      if(data[4]=="Noise - Vehicle"){
+        type6=type6+1;
+      }
+      if(data[6]=="Club/Bar/Restaurant"){
+        l_type6=l_type6+1;
+      }
     }
 
-
   }
+
+  var factor=0.3;
+  var factor2=0.3;
+
 
   //fill(255,255,255,80);
   //rect(20 - width / 2, 20 - height / 2,600,200);
@@ -121,28 +183,49 @@ function setup() {
   fill(255, 0, 0);
   text("7PM",320 - width / 2, 200 - height / 2);
 
+  var h=600;
 
-  fill(255, 255, 0, 40);
+  fill(255, 255, 0, 60);
   rect(20 - width / 2, 150 - height / 2, 60, 40); 
-  fill(255, 200, 0, 40);
+  rect(20 - width / 2, 300 - height / 2,type1*factor,20);
+  rect(20 - width / 2, 400 - height / 2,l_type1*factor2,20);
+
+  fill(255, 200, 0, 60);
   rect(80 - width / 2, 150 - height / 2, 60, 40);
-  fill(255, 150, 0, 40);
+  rect(20 - width / 2+type1*factor, 300 - height / 2,type2*factor,20);
+  rect(20 - width / 2+l_type1*factor2, 400 - height / 2,l_type2*factor2,20);
+
+  fill(255, 150, 0, 60);
   rect(140 - width / 2, 150 - height / 2, 60, 40);
-  fill(255, 100, 0, 40);
+  rect(20 - width / 2+type1*factor+type2*factor, 300 - height / 2,type3*factor,20);
+  rect(20 - width / 2+l_type1*factor2+l_type2*factor2, 400 - height / 2,l_type3*factor2,20);
+
+  fill(255, 100, 0, 60);
   rect(200 - width / 2, 150 - height / 2, 60, 40);
-  fill(255, 50, 0, 40);
+  rect(20 - width / 2+type1*factor+type2*factor+type3*factor, 300 - height / 2,type4*factor,20);
+  rect(20 - width / 2+l_type1*factor2+l_type2*factor2+l_type3*factor2, 400 - height / 2,l_type4*factor2,20);
+
+  fill(255, 50, 0, 60);
   rect(260 - width / 2, 150 - height / 2, 60, 40);
-  fill(255, 0, 0, 40);
+  rect(20 - width / 2+type1*factor+type2*factor+type3*factor+type4*factor, 300 - height / 2,type5*factor,20);
+  rect(20 - width / 2+l_type1*factor2+l_type2*factor2+l_type3*factor2+l_type4*factor2, 400 - height / 2,l_type5*factor2,20);
+
+  fill(255, 0, 0, 60);
   rect(320 - width / 2, 150 - height / 2, 60, 40);
+  rect(20 - width / 2+type1*factor+type2*factor+type3*factor+type4*factor+type5*factor, 300 - height / 2,type6*factor,20);
+  rect(20 - width / 2+l_type1*factor2+l_type2*factor2+l_type3*factor2+l_type4*factor2+l_type5*factor2, 400 - height / 2,l_type6*factor2,20);
 
 
-  // textStyle(BOLD);
-  // fill(255,100,0,200)
-  // text("Compliant Type: Vehicle Compliant", 20 - width / 2, 300 - height / 2);
-  // text("Compliant Location: Street/Sidewalk", 20 - width / 2, 500 - height / 2);
+
+  textStyle(BOLD);
+  textSize(20)
+  fill(255,100,0,200)
+  text("Compliant Type: Vehicle Compliant by Time", 20 - width / 2, 300 - height / 2);
+  text("Compliant Location: Street/Sidewalk by Time", 20 - width / 2, 400 - height / 2);
+  
   
 
-  
+
 
 
   
